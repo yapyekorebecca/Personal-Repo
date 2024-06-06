a3d6b018974d270f9e6d79032d19517bf6bcfc1e
yapyekorebecca <yapyekor@gmail.com>
2024-06-06 16:11:02 +0300

    i added some code

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -13,6 +13,36 @@ class Student:
 std=Student("Alison",20)
 print(std.name)
 print(std.age)
+
+#__str__() function controls what should be returned when the class object is represented as a string
+class Student:
+    def __init__(person,name,age):
+        person.name=name
+        person.age=age
+        
+std=Student("Alison",20)
+print(std)   #CHECK THIS OUT, it will return  <__main__.Student object at 0x000001A31F9C6030>
+
+#Using __str__() function 
+
+class Animal:
+    def __init__(breed,state,status):
+        breed.state=state
+        breed.status=status
+    def __str__(breed): 
+        return f"{breed.state}({breed.status})"  
+    def printDetails(breed):
+        print(breed.status + " and its from " + breed.state)
+    
+mammal=Animal("Texas", "healthy")
+mammal.state="Calabasas"
+
+print(mammal)
+mammal.printDetails()
+
+
+
+
         
 
         
