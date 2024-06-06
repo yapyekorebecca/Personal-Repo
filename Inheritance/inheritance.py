e6724c79c3fd760118ed9676b3005ebd2cba99ef
yapyekorebecca <yapyekor@gmail.com>
2024-06-06 15:05:50 +0300

    Josciuaatw

--- Inheritance/inheritance.py
+++ Inheritance/inheritance.py
@@ -1,14 +1,19 @@
-# class Student:
-#     age=16
+class Student:
+    age=16
     
-#     std=Student()
-#     print(std.age)
+std=Student()
+print(std.age)
+
+# classes and objects in python
 class Student:
     def __init__(person,name,age):
         person.name=name
         person.age=age
         
-        std=Student("Alison","20")
-        print(std.name)
-        print(std.age)
+std=Student("Alison",20)
+print(std.name)
+print(std.age)
+        
+
+        
          
